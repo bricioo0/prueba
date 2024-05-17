1b9fbd295baf468021b44405fca41af1e16bbad9
bricio <briciopincha@gmail.com>
2024-05-17 19:51:01 -0300

    jsjsj

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('iniciar-sesion').addEventListener('click', function() {
+    alert('Iniciar Sesión clicked');
+    window.location.href ="iniciarsesion.html"
+});
+
+document.getElementById('registrar').addEventListener('click', function() {
+    alert('Registrarte clicked');
+    window.location.href="registrar.html"
+});
