cbb03d870bb566fdcd527f1bd5c460e2321f1533
bricio <briciopincha@gmail.com>
2024-05-31 16:14:44 -0300

    a vee

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 document.getElementById('iniciar-sesion').addEventListener('click', function() {
-    alert('Iniciar Sesión clicked');
-    window.location.href ="iniciarsesion.html"
+  window.location.href ="iniciarsesion.html"
 });
 
 document.getElementById('registrar').addEventListener('click', function() {
-    alert('Registrarte clicked');
-    window.location.href="resgitrar.html"
+   window.location.href="resgitrar.html"
 });
